eba3a2d6a0d94d439b313df15d57faa939452743
bondarsasha <bondaralexandr1991@gmail.com>
2017-11-13 06:24:03 +0200

    xml

---
+++ user.js
@@ -0,0 +1,38 @@
+function getNumber() {
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'phones.json', true);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    button.parentNode.removeChild(button);
+
+    if (xhr.status != 200) {
+      
+      alert(xhr.status + ': ' + xhr.statusText);
+    } else {
+      try {
+        var phones = JSON.parse(xhr.responseText);
+      } catch (e) {
+        alert("Некорректный ответ " + e.message);
+      }
+      showPhones(phones);
+    }
+
+  }
+
+  xhr.send();
+
+  button.innerHTML = 'Загружаю...';
+  button.disabled = true;
+}
+
+function showPhones(phones) {
+
+  phones.forEach(function(phone) {
+    var li = list.appendChild(document.createElement('li'));
+    li.innerHTML = phone.name;
+  });
+}
